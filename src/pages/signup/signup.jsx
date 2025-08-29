b7c0d11391bf34b3847e2ea077acd32b3591f073
Prince Owire <princeowire131@gmail.com>
2025-08-29 15:29:51 +0100

    hello

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -29,7 +29,7 @@ export default function Signup() {
 
         {/* Right Side Form */}
         {loading ? (
-          <div className="flex items-center justify-center w-1/2 min-h-[450px] bg-gray-900">
+          <div className="flex items-center justify-center w-1/2 min-h-[476px] bg-gray-900">
             <div className="w-10 h-10 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin"></div>
           </div>
         ) : (
